dc1f3b2e6691f3294a46d332007ff16e26ccdcd9
Bhargavsinh  M Barad <118417960+BhargavsinhBarad@users.noreply.github.com>
2023-05-03 10:27:43 +0530

    Appmania

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:ecommerce_app/utils/pr.dart';
+
+import 'package:flutter/material.dart';
+
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+
+import 'Screens/homepage.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "ECommerce App ",
+        home: Home()),
+  );
+}
